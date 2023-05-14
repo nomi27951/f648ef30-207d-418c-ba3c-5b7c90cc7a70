6792cab3bf80ddeb16f74795326989d152828ec8
Nauman <nomi27951@gmail.com>
2023-05-14 22:33:15 +0200

    Question 1 fix

--- Assets/_Complete-Game/Scripts/LevelData.cs
+++ Assets/_Complete-Game/Scripts/LevelData.cs
@@ -1,20 +1,15 @@
 using UnityEngine;
 
-namespace GameDesign
+[CreateAssetMenu(fileName = "LevelData", menuName = "ScriptableObjects/LevelData", order = 1)]
+public class LevelData : ScriptableObject
 {
-    [CreateAssetMenu(fileName = "LevelData", menuName = "ScriptableObjects/LevelData", order = 1)]
-    public class LevelData : ScriptableObject
+    public const int DefaultEnemyCount = 5;
+    public int[] enemiesPerLevel = new int[]{};
+    public Vector3 playerStartPos = new Vector3 (0, 0, 0f);
+    public Vector3 exitPos = new Vector3 (7, 7, 0f);
+
+    public int GetEnemiesPerLevel(int level)
     {
-        public int[] enemiesPerLevel = new int[] { };
-        public Vector3 playerStartPos = new Vector3(0, 0, 0f);
-        public Vector3 exitPos = new Vector3(7, 7, 0f);
-
-        public int GetEnemiesPerLevel(int level)
-        {
-            level = Mathf.Max(1, level);
-            level = Mathf.Min(level, enemiesPerLevel.Length);
-
-            return enemiesPerLevel[level - 1] > 0 ? enemiesPerLevel[level - 1] : (int)Mathf.Log(level, 2f);
-        }
+        return level <= enemiesPerLevel.Length ? enemiesPerLevel[level - 1] : (int)Mathf.Log(level, 2f);
     }
 }
